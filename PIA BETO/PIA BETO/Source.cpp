9bbd8239eec826392ade67006ef904de3f3c8e54
Silver25Life <112659585+Silver25Life@users.noreply.github.com>
2022-10-25 12:27:18 -0500

    Avance v1.1.2
    "Mejora" de código, si va a revisar el segundo avance del pia, vea la v1.1.1.

--- PIA BETO/PIA BETO/Source.cpp
+++ PIA BETO/PIA BETO/Source.cpp
@@ -4,8 +4,9 @@
 #include<string>
 #include<conio.h>
 using namespace std;
-int op[1], i, j = 0;
-bool val = true, val2 = true, val3 = true;
+bool val = true;
+char op[1];
+int i, j = 0;
 char a = 0;
 struct videojuego{
 	char yearc[4], pricec[9];
@@ -14,9 +15,10 @@ struct videojuego{
 	float price, total, taxf;
 }juego[100];
 void agregar() {
-	while (val && j != 101) {
+	bool votro = true, nuevo = true;
+	while (nuevo && j != 101) {
 		juego[j].ID = j+1;
-		cout << "\tEscriba el nombre del juego número " << j+1 << ": ";
+		cout << "\n\tEscriba el nombre del juego número " << j+1 << ": ";
 		cin.ignore();
 		getline(cin, juego[j].name);
 		cout << "\tEscriba el año de salida: ";
@@ -63,195 +65,190 @@ void agregar() {
 		juego[j].total = juego[j].taxf + juego[j].price;
 		cout << "\tEl impuesto es del 16%, entonces el precio total es de: $" << juego[j].total << ", con un impuesto del: $" << juego[j].taxf << endl;
 		j++;
-		while (val2) {
+		while (votro) {
 			cout << "\n\n\t ¿Le gustaria agregar otro juego?\n\t 1.Si\n\t 2.No\n\t";
-			cin >> op[0];
-			switch (op[0]) {
-			case 1:
-				val2 = false;
-				break;
-			case 2:
-				val = false;
-				val2 = false;
-				break;
-			default:
+			op[0] = _getche();
+			if (op[0] < 49 || op[0]>50) {
 				cout << "\n\t Por favor, elija una opcion valida.";
 			}
+			else {
+				switch (op[0]) {
+				case '1':
+					votro = false;
+					break;
+				case '2':
+					nuevo = false;
+					votro = false;
+					break;
+				}
+			}
 		}
-		val2 = true;
+		votro = true;
 	}
-	if (val != false) {
+	if (j>100) {
 		cout << "\n\n\t Ya esta llena la lista de juegos" << endl;
 	}
 }
 
 void modificar() {
-	int unsigned ID, h;
-	bool valex = false,m=true;
-	while (val) {
-		cout << "\t\tFavor de ingresar el ID del juego que busca: ";
+	bool mod = true, vID = false, vmod = true, omod = true;
+	int unsigned ID;
+	while (mod) {
+		cout << "\n\tFavor de ingresar el ID del juego que busque modificar y presione Enter: ";
 		cin >> ID;
 		for (int i = 0; i < j; i++) {
 			if (juego[i].ID == ID) {
-				valex = true;
-				h = i;
-				m = true;
-			}
-			else {
-				m = false;
+				vID = true;
+				i = j;
 			}
 		}
-		if (valex==true) {
+		if (vID == false) {
+			cout << "\n\t\tNo se encontro esa ID, favor de revisar el listado.";
+			mod = false;
+		}
+		else if (vID == true) {
+			vID = false;
 			cout << "\t\tID\tNombre\tAño\tClasificación\tCarácteristicas\t\t\t\tDescripción\t\t\t\tGenero\tPrecio\tImpuesto\tPrecio Total" << endl;
-			cout << "\t\t" << juego[h].ID << "\t\t" << juego[h].name << "\t" << juego[h].year << "\t" << juego[h].clasif << "\t" << juego[h].caract << "\t" << juego[h].desc << "\t" << juego[h].gen << "\t";
-			cout << juego[h].price << "\t" << juego[h].taxf << "\t" << juego[h].total << endl;
-			while (val2) {
+			cout << "\t\t" << juego[ID-1].ID << "\t\t" << juego[ID-1].name << "\t" << juego[ID-1].year << "\t" << juego[ID-1].clasif << "\t" << juego[ID-1].caract << "\t" << juego[ID-1].desc << "\t" << juego[ID-1].gen << "\t";
+			cout << juego[ID-1].price << "\t" << juego[ID-1].taxf << "\t" << juego[ID-1].total << endl;
+			while (vmod) {
 				cout << "\n\t¿Este es el juego que quiere modificar?\n\t1.Si\n\t2.No\n\t";
-				cin >> op[0];
-				switch (op[0]) {
-				case 1:
-					cout << "\tEscriba el nombre del juego número " << h + 1 << ": ";
-					cin.ignore();
-					getline(cin, juego[h].name);
-					cout << "\tEscriba el año de salida: ";
-					cin >> juego[h].yearc;
-					for (int i = 0; i < 4; i++) {
-						if (juego[h].yearc[i] == 0) {
-							i = 4;
-						}
-						else if (juego[h].yearc[i] < 48 || juego[h].yearc[i] > 57) {
-							for (int k = 0; k < 4; k++) {
-								juego[h].yearc[k] = 0;
-							}
-							cout << "\n\t Favor de escribir un año valido sin letras ni espacios, el año solo puede ser de 4 digitos: ";
-							cin >> juego[h].yearc;
-							i = -1;
-						}
-					}
-					juego[j].year = atoi(juego[h].yearc);
-					cin.ignore();
-					cout << "\tEscriba la clasificación del juego: ";
-					getline(cin, juego[h].clasif);
-					cout << "\tEscriba las carácteristicas del juego: ";
-					getline(cin, juego[h].caract);
-					cout << "\tEscriba la descripción del juego: ";
-					getline(cin, juego[h].desc);
-					cout << "\tEscriba el genero del juego: ";
-					getline(cin, juego[h].gen);
-					cout << "\tEscriba el precio del juego: ";
-					cin >> juego[h].pricec;
-					for (int i = 0; i < 9; i++) {
-						if (juego[h].pricec[i] == 0) {
-							i = 9;
-						}
-						else if (juego[h].pricec[i] != 46 && (juego[h].pricec[i] < 48 || juego[h].pricec[i] > 57)) {
-							for (int k = 0; k < 9; k++) {
-								juego[h].pricec[k] = 0;
-							}
-							cout << "\n\t Favor de escribir un precio valido sin letras ni espacios, el precio solo puede ser de 8 digitos y un punto: ";
-							cin >> juego[h].pricec;
-							i = -1;
-						}
-					}
-					juego[h].price = atof(juego[h].pricec);
-					juego[h].taxf = juego[h].price * 0.16;
-					juego[h].total = juego[h].taxf + juego[h].price;
-					cout << "\tEl impuesto es del 16%, entonces el precio total es de: $" << juego[h].total << ", con un impuesto del: $" << juego[h].taxf << endl;
-					while (val3) {
-						cout << "\n\t¿Le gustaria modificar otro juego?\n\t1.Si\n\t2.No\n\t";
-						cin >> op[0];
-						switch (op[0]) {
-						case 1:
-							val2 = false;
-							val3 = false;
-							break;
-						case 2:
-							val = false;
-							val2 = false;
-							val3 = false;
-							break;
-						default:
-							cout << "\n\t Favor de elegir una opcion valida.";
-						}
-					}
-					val3 = true;
-					break;
-				case 2:
-					valex = false;
-					val2 = false;
-					break;
-				default:
+				op[0] = _getche();
+				if (op[0] < 49 || op[0]>50) {
 					cout << "\n\tFavor de elegir una opcion valida";
-					break;
+				}
+				else {
+					vmod = false;
 				}
 			}
-			val2 = true;
-		}
-		if (valex == false && m == false) {
-			cout << "\n\t\tNo se encontro esa ID, favor de revisar el listado.";
-			val = false;
+			switch (op[0]) {
+			case '1':
+				cout << "\tEscriba el nombre del juego número " << ID << ": ";
+				cin.ignore();
+				getline(cin, juego[ID - 1].name);
+				cout << "\tEscriba el año de salida: ";
+				cin >> juego[ID - 1].yearc;
+				for (int i = 0; i < 4; i++) {
+					if (juego[ID - 1].yearc[i] == 0) {
+						i = 4;
+					}
+					else if (juego[ID - 1].yearc[i] < 48 || juego[ID - 1].yearc[i] > 57) {
+						for (int k = 0; k < 4; k++) {
+							juego[ID - 1].yearc[k] = 0;
+						}
+						cout << "\n\t Favor de escribir un año valido sin letras ni espacios, el año solo puede ser de 4 digitos: ";
+						cin >> juego[ID - 1].yearc;
+						i = -1;
+					}
+				}
+				juego[j].year = atoi(juego[ID - 1].yearc);
+				cin.ignore();
+				cout << "\tEscriba la clasificación del juego: ";
+				getline(cin, juego[ID - 1].clasif);
+				cout << "\tEscriba las carácteristicas del juego: ";
+				getline(cin, juego[ID - 1].caract);
+				cout << "\tEscriba la descripción del juego: ";
+				getline(cin, juego[ID - 1].desc);
+				cout << "\tEscriba el genero del juego: ";
+				getline(cin, juego[ID - 1].gen);
+				cout << "\tEscriba el precio del juego: ";
+				cin >> juego[ID - 1].pricec;
+				for (int i = 0; i < 9; i++) {
+					if (juego[ID - 1].pricec[i] == 0) {
+						i = 9;
+					}
+					else if (juego[ID - 1].pricec[i] != 46 && (juego[ID - 1].pricec[i] < 48 || juego[ID - 1].pricec[i] > 57)) {
+						for (int k = 0; k < 9; k++) {
+							juego[ID - 1].pricec[k] = 0;
+						}
+						cout << "\n\t Favor de escribir un precio valido sin letras ni espacios, el precio solo puede ser de 8 digitos y un punto: ";
+						cin >> juego[ID - 1].pricec;
+						i = -1;
+					}
+				}
+				juego[ID - 1].price = atof(juego[ID - 1].pricec);
+				juego[ID - 1].taxf = juego[ID - 1].price * 0.16;
+				juego[ID - 1].total = juego[ID - 1].taxf + juego[ID - 1].price;
+				cout << "\tEl impuesto es del 16%, entonces el precio total es de: $" << juego[ID - 1].total << ", con un impuesto del: $" << juego[ID - 1].taxf << endl;
+				while (omod) {
+					cout << "\n\t¿Le gustaria modificar otro juego?\n\t1.Si\n\t2.No\n\t";
+					op[0] = _getche();
+					if (op[0] < 49 || op[0]>50) {
+						cout << "\n\t Favor de elegir una opcion valida.";
+					}
+					else {
+						omod = false;
+					}
+				}
+				omod = true;
+				if (op[0] = '2') {
+					mod = false;
+				}
+				break;
+			case '2':
+				mod = false;
+				break;
+			}
 		}
 	}
 }
 
 void eliminar() {
+	bool del = true, vID = false, vdel = true;
 	int unsigned ID, h;
 	bool valex = false;
-	while (val) {
-		cout << "\t\tFavor de ingresar el ID del juego que busca: ";
+	while (del) {
+		cout << "\n\tFavor de ingresar el ID del juego que quiera eliminar y presione Enter: ";
 		cin >> ID;
 		for (int i = 0; i < j; i++) {
 			if (juego[i].ID == ID) {
-				valex = true;
-				h = i;
+				vID = true;
 			}
 		}
-		if (valex == true) {
+		if (vID == false) {
+			cout << "\n\t\tNo se encontro esa ID, favor de revisar el listado.\n";
+			del = false;
+		}
+		else if (vID == true) {
+			vID = false;
 			cout << "\t\tID\tNombre\tAño\tClasificación\tCarácteristicas\t\t\t\tDescripción\t\t\t\tGenero\tPrecio\tImpuesto\tPrecio Total" << endl;
-			cout << "\t\t" << juego[h].ID << "\t\t" << juego[h].name << "\t" << juego[h].year << "\t" << juego[h].clasif << "\t" << juego[h].caract << "\t" << juego[h].desc << "\t" << juego[h].gen << "\t";
-			cout << juego[h].price << "\t" << juego[h].taxf << "\t" << juego[h].total << endl;
-			while (val2) {
+			cout << "\t\t" << juego[ID - 1].ID << "\t\t" << juego[ID - 1].name << "\t" << juego[ID - 1].year << "\t" << juego[ID - 1].clasif << "\t" << juego[ID - 1].caract << "\t" << juego[ID - 1].desc << "\t" << juego[ID - 1].gen << "\t";
+			cout << juego[ID - 1].price << "\t" << juego[ID - 1].taxf << "\t" << juego[ID - 1].total << endl;
+			while (vdel) {
 				cout << "\n\t¿Este es el juego que quiere eliminar?\n\t1.Si\n\t2.No\n\t";
-				cin >> op[0];
-				switch (op[0]) {
-				case 1:
-					for (int i = h; i < j; i++) {
-						juego[h].caract = juego[h + 1].caract;
-						juego[h].clasif = juego[h + 1].clasif;
-						juego[h].desc = juego[h + 1].desc;
-						juego[h].desc = juego[h + 1].desc;
-						juego[h].gen = juego[h + 1].gen;
-						juego[h].name = juego[h + 1].name;
-						juego[h].price = juego[h + 1].price;
-						juego[h].taxf = juego[h + 1].taxf;
-						juego[h].total = juego[h + 1].total;
-						juego[h].year = juego[h + 1].year;
-						cout << "\n\t Juego eliminado con exito";
-					}
-					juego[j-1].ID = 0;
-					j--;
-					val = false;
-					val2 = false;
-					break;
-				case 2:
-					val2 = false;
-					break;
-				default:
-					cout << "\n\tFavor de elegir una opcion valida";
-					break;
+				op[0] = _getche();
+				if (op[0] < 49 || op[0]>50) {
+					cout << "\n\tFavor de elegir una opcion valida\n";
+				}
+				else {
+					vdel = false;
 				}
 			}
-			val2 = true;
-		}
-		if (valex == false) {
-			cout << "\n\t\tNo se encontro esa ID, favor de revisar el listado.";
-			val = false;
+			vdel = true;
+			if (op[0] == '1') {
+				for (int i = ID - 1; i < j; i++) {
+					juego[i].caract = juego[i + 1].caract;
+					juego[i].clasif = juego[i + 1].clasif;
+					juego[i].desc = juego[i + 1].desc;
+					juego[i].desc = juego[i + 1].desc;
+					juego[i].gen = juego[i + 1].gen;
+					juego[i].name = juego[i + 1].name;
+					juego[i].price = juego[i + 1].price;
+					juego[i].taxf = juego[i + 1].taxf;
+					juego[i].total = juego[i + 1].total;
+					juego[i].year = juego[i + 1].year;
+				}
+				cout << "\n\t Juego eliminado con exito\n";
+				juego[j - 1].ID = 0;
+				j--;
+			}
+			del = false;
 		}
 	}
 }
 
 void lista() {
-	cout << "\t\tID\tNombre\tAño\tClasificación\tCarácteristicas\t\t\t\tDescripción\t\t\t\tGenero\tPrecio\tImpuesto\tprecio total" << endl;
+	cout << "\n\t\tID\tNombre\tAño\tClasificación\tCarácteristicas\t\t\t\tDescripción\t\t\t\tGenero\tPrecio\tImpuesto\tprecio total" << endl;
 	for (int i = 0; i < j; i++) {
 		cout << "\t\t" << juego[i].ID << "\t\t" << juego[i].name << "\t" << juego[i].year << "\t" << juego[i].clasif << "\t" << juego[i].caract << "\t" << juego[i].desc << "\t" << juego[i].gen << "\t";
 		cout << juego[i].price << "\t" << juego[i].taxf << "\t" << juego[i].total << endl;
@@ -259,7 +256,6 @@ void lista() {
 }
 
 void main() {
-
 	setlocale(LC_ALL, "");
 	while (val) {
 		cout << "\n\t========== Menú ==========" << endl;
@@ -269,36 +265,39 @@ void main() {
 		cout << "\t4. Mostrar articulos." << endl;
 		cout << "\t5. Limpiar pantalla." << endl;
 		cout << "\t6. Salir." << endl;
-		cout << "\t= = = Elija una de las opciones anteriores y presione enter por favor = = =\n\t\t";
-		cin >> op[0];
-
-		switch (op[0]) {
-		case 1:
-			agregar();
-			val = true;
-			break;
-		case 2:
-			modificar();
-			val = true;
-			break;
-		case 3:
-			eliminar();
-			val = true;
-			break;
-		case 4:
-			lista();
-			break;
-		case 5:
-			system("cls");
-			break;
-		case 6:
-			cout << "Saliendo del programa, favor de presionar cualquier tecla...";
-			val = false;
-			break;
-		default:
-			cout << "Favor de elegir una de las anteriores opciones.";
-			break;
+		cout << "\t= = = Elija una de las opciones anteriores = = =\n\t\t";
+		op[0] = _getche();
+		if (op[0] < 49 || op[0]>54) {
+			cout << "\n\t Favor de elegir una de las anteriores opciones.";
+			op[0] = 0;
+		}
+		else {
+			switch (op[0]) {
+			case '1':
+				agregar();
+				val = true;
+				break;
+			case '2':
+				modificar();
+				val = true;
+				break;
+			case '3':
+				eliminar();
+				val = true;
+				break;
+			case '4':
+				lista();
+				break;
+			case '5':
+				system("cls");
+				break;
+			case '6':
+				cout << "\n\t Saliendo del programa...\n\t";
+				val = false;
+				break;
+			}
 		}
 	}
+	system("pause");
 
 }
